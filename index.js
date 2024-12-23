632f6dba4e22b53cda9335cd7c0bf5c63b5e4e77
Mukesh Kumar <mukesh.kumar@borderfree.io>
2024-12-23 10:54:02 +0530

    commented

--- index.js
+++ index.js
@@ -9,36 +9,36 @@ app.use(cors());
 const Gift = require("./giftModel");
 
 connectDB();
-app.post("/selectgift", async (req, res) => {
-  try {
-    const { giftName, userName } = req.body;
+// app.post("/selectgift", async (req, res) => {
+//   try {
+//     const { giftName, userName } = req.body;
 
-    if (!giftName || !userName) {
-      return res.status(400).json({
-        success: false,
-        message: "Both giftName and userName are required..",
-      });
-    }
+//     if (!giftName || !userName) {
+//       return res.status(400).json({
+//         success: false,
+//         message: "Both giftName and userName are required..",
+//       });
+//     }
 
-    // Create the gift entry in the database
-    const gift = await Gift.create({ giftName, userName });
+//     // Create the gift entry in the database
+//     const gift = await Gift.create({ giftName, userName });
 
-    // Respond with success
-    return res.status(201).json({
-      success: true,
-      message: "Gift successfully created!",
-      data: gift, // Optional: Include the created gift details
-    });
-  } catch (error) {
-    console.error("Error creating gift:", error.message);
+//     // Respond with success
+//     return res.status(201).json({
+//       success: true,
+//       message: "Gift successfully created!",
+//       data: gift, // Optional: Include the created gift details
+//     });
+//   } catch (error) {
+//     console.error("Error creating gift:", error.message);
 
-    // Handle database-specific or internal errors
-    return res.status(500).json({
-      success: false,
-      message: "An error occurred while processing your request.",
-    });
-  }
-});
+//     // Handle database-specific or internal errors
+//     return res.status(500).json({
+//       success: false,
+//       message: "An error occurred while processing your request.",
+//     });
+//   }
+// });
 app.get("/", (req, res) => {
   res.send("Hi welcome to birthday wisher baby");
 });
